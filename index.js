1aaabed4ea9a3ecd794b23a42aa00535644a1ead
devarahifz <devara.hifzhurrahman22@gmail.com>
2022-05-14 01:51:58 +0700

    api-auth

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const passport = require('passport');
 
 const app = express();
 const productRoutes = require('./src/routes/products');
-const authRoutes = require('./src/routes/auth');
+const userRoutes = require('./src/routes/user');
 const blogRoutes = require('./src/routes/blog');
+const path = require('path');
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + '-' + file.originalname);
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,7 +45,7 @@ app.use((req, res, next) => {
 
 
 app.use('/v1/customers', productRoutes)
-app.use('/v1/auth', authRoutes)
+app.use('/api/users', userRoutes)
 app.use('/v1/blog', blogRoutes)
 
 app.use((error, req, res, next) => {
@@ -29,8 +56,15 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message, data: data });
 })
 
-mongoose.connect('mongodb+srv://user:rahasia@cluster0.j5ing.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true })
+const db = require('./src/config/keys').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
   .then(() => {
     app.listen(4000, () => console.log('Server is running on port 4000'));
   })
   .catch(err => console.log(err))
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./src/config/passport")(passport);
---
+++ src/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://user:rahasia@cluster0.j5ing.mongodb.net/bentol?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
